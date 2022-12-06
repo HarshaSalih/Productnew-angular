44ac17ec47334116bd012dbe6fc932a9aa58ae0d
HarshaSalih <harshasalih2000@gmail.com>
2022-12-06 21:44:38 +0530

    commits

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-product',
@@ -16,11 +17,34 @@ export class AddProductComponent {
   sellerName=""
   distributorName=""
 
+  constructor(private api:ApiService){}
+
   readValues=()=>
   {
     let data:any={"productCode":this.productCode,"ProductName":this.productName,"man_date":this.man_date,"exp_date":this.exp_date,"brand":this.brand,"price":this.price,"sellerName":this.sellerName,"distributorName":this.distributorName}
     console.log(data)
-  }
+    this.api.addProduct(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if (response.status=="success")
+        {
+          alert("Product added successfully")
+          this.productCode=""
+          this.productName=""
+          this.man_date=""
+          this.exp_date=""
+          this.brand=""
+          this.price=""
+          this.sellerName=""
+          this.distributorName=""
 
+        }
+        else{
+          alert("Something went wrong")
+        }
+  }
+    )
+}
 
 }
